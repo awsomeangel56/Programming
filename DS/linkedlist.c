87b1178a72571c51b90b78d5fab158c576a06983
awsomeangel56 <bilalb162004@gmail.com>
2024-11-17 09:52:48 +0530

    Add files via upload

--- DS/linkedlist.c
+++ DS/linkedlist.c
@@ -1,98 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node {
+struct Node{
     int data;
-    struct Node* next;
+    struct Node* next;    
 };
-
-void insertAtEnd(struct Node** head, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* last = *head;
-    newNode->data = data;
-    newNode->next = NULL;
-    if (*head == NULL) {
-        *head = newNode;
+void insertatend(struct Node** head, int data){
+    struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* last= *head;
+    newNode->data=data;
+    newNode->next=NULL;
+    if(*head==NULL){
+        *head=newNode;
         return;
     }
-    while (last->next != NULL) {
-        last = last->next;
+    while(last->next!=NULL){
+        last=last->next;
     }
-    last->next = newNode;
+    last->next=newNode;
 }
-
-void displayList(struct Node* node) {
-    while (node != NULL) {
-        printf("%d -> ", node->data);
-        node = node->next;
+void displayList(struct Node* node){
+    while(node!=NULL){
+        printf("%d -> ",node->data);
+        node=node->next;
     }
-    printf("NULL\n");
+    printf("\n");
 }
-
-struct Node* mergeLists(struct Node* head1, struct Node* head2) {
-    if (head1 == NULL) return head2;
-    if (head2 == NULL) return head1;
-
-    if (head1->data < head2->data) {
-        head1->next = mergeLists(head1->next, head2);
+struct Node* mergeList(struct Node* head1, struct Node* head2){
+    if(head1==NULL){
+        return head2;
+    }
+    if(head2==NULL){
         return head1;
-    } else {
-        head2->next = mergeLists(head1, head2->next);
+    }
+    if(head1->data<head2->data){
+        head1->next=mergeList(head1->next,head2);
+        return head1;
+    }
+    else{
+        head2->next=mergeList(head1,head2->next);
         return head2;
     }
 }
-
-int main() {
-    struct Node* head1 = NULL;
-    struct Node* head2 = NULL;
-    struct Node* mergedHead = NULL;
-    int choice, data;
-
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Insert in List 1\n");
-        printf("2. Insert in List 2\n");
-        printf("3. Display List 1\n");
-        printf("4. Display List 2\n");
-        printf("5. Merge Lists\n");
-        printf("6. Display Merged List\n");
-        printf("7. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter data to insert in List 1: ");
-                scanf("%d", &data);
-                insertAtEnd(&head1, data);
-                break;
-            case 2:
-                printf("Enter data to insert in List 2: ");
-                scanf("%d", &data);
-                insertAtEnd(&head2, data);
-                break;
-            case 3:
-                printf("List 1: ");
-                displayList(head1);
-                break;
-            case 4:
-                printf("List 2: ");
-                displayList(head2);
-                break;
-            case 5:
-                mergedHead = mergeLists(head1, head2);
-                printf("Lists merged successfully.\n");
-                break;
-            case 6:
-                printf("Merged List: ");
-                displayList(mergedHead);
-                break;
-            case 7:
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
+int main(){
+    int ch,data;
+    struct Node* head1=NULL;
+    struct Node* head2=NULL;
+    struct Node* Mergedhead=NULL;
+    do{
+        printf("\n1. insert at list 1\n2. insert at list 2\n3. display list 1\n4. display list 2\n5. merge lists\n6. display merged list\n7. exit\nenter your choice: ");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: printf("Enter element: ");
+            scanf("%d",&data);
+            insertatend(&head1,data);
+            break;
+            case 2: printf("Enter element: ");
+            scanf("%d",&data);
+            insertatend(&head2,data);
+            break;
+            case 3: printf("List 1: \n");
+            displayList(head1);
+            break;
+            case 4: printf("List 2: \n");
+            displayList(head2);
+            break;
+            case 5: Mergedhead=mergeList(head1,head2);
+            printf("Lists merged successfully !!");
+            break;
+            case 6: printf("Merged Lis: \n");
+            displayList(Mergedhead);
+            break;
+            case 7: printf("Exitting program...");
+            break;
+            default: printf("Invalid choice !!");       
         }
-    }
-
-    return 0;
+    }while(ch!=7);
 }
