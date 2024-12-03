46181226124266534a38e4500088e50ddb0509ef
awsomeangel56 <bilalb162004@gmail.com>
2024-12-03 12:27:07 +0530

    Add files via upload

--- JAVA SEM/oddevenpack.java
+++ JAVA SEM/oddevenpack.java
@@ -1,18 +1,36 @@
-package evenpackage;
-public class Even{
-    public static void oddoreven(int num){
-        if(num%2==0){
-            System.out.print("Number is even");
+class Shared{
+    private boolean isOdd=true;
+    synchronized void print(int num){
+    while(isOdd!=(num%2!=0)){
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            System.out.print(e);
         }
-        else{
-            System.out.println("Number is odd");
+    }
+    System.out.print(num+" ");
+    isOdd=!isOdd;
+    notify();
+    }
+}
+class NumberThread extends Thread{
+    private final Shared shared;
+    private final int start;
+    NumberThread(Shared shared, int start){
+        this.shared=shared;
+        this.start=start;
+    }
+    public void run(){
+        for(int i=start;i<=100;i+=2){
+            shared.print(i);
         }
     }
 }
-import evenpackage.Even;
 public class oddevenpack{
     public static void main(String[] args) {
-        Even obj=new Even();
-        obj.oddoreven(15);
+        Shared shared=new Shared();
+        new NumberThread(shared,1).start();
+        new NumberThread(shared,2).start();
+
     }
 }
