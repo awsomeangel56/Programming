f93bfa56922c1f44a7d1ef4f0614d6d2d017f5d1
awsomeangel56 <bilalb162004@gmail.com>
2025-03-11 22:36:21 +0530

    Add files via upload

---
+++ OS/sjf.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int bt[50],wt[50],tat[50],total,pid[50];
+void sort(){
+    int temp;
+    for(int i=0;i<total-1;i++){
+        for(int j=0;j<total-i-1;j++){
+            if(bt[j]>bt[j+1]){
+                temp=bt[j];
+                bt[j]=bt[j+1];
+                bt[j+1]=temp;
+            }
+        }
+    }
+}
+void findwt(){
+    wt[0]=0;
+    for(int i=1;i<total;i++)
+        wt[i]=bt[i-1]+wt[i-1];
+}
+void findtat(){
+    for(int i=0;i<total;i++)
+        tat[i]=bt[i]+wt[i];
+}
+void printGanttChart() {
+    printf("\nGantt Chart:\n ");
+    int t = 0;
+    printf("|");
+    for (int i = 0; i < total; i++) {
+        printf(" P%d |", pid[i]);
+        t += bt[i];
+    }
+    printf("\n0");
+    t = 0;
+    for (int i = 0; i < total; i++) {
+        t += bt[i];
+        printf("     %d", t);
+    }
+    printf("\n");
+}
+void findat(){
+    int totalwt=0,totaltat=0;
+        printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+        for(int i=0;i<total;i++){
+            printf("\nP%d\t\t%d\t\t%d\t\t%d\t\t",i,bt[i],wt[i],tat[i]);
+            totalwt+=wt[i];
+            totaltat+=tat[i];
+        }
+        float w=(float)totalwt/total;
+        float t=(float)totaltat/total;
+        printf("\ntotal waiting time = %d \n",totalwt);
+        printf("total turn around time =%d \n",totaltat);
+        printf("average wt=%f \n average tat=%f \n",w,t);
+}
+void main(){
+    printf("enter number of process\n");
+    scanf("%d",&total);
+    printf("enter burst time\n");
+    for(int i=0;i<total;i++){
+        pid[i]=i+1;
+        scanf("%d",&bt[i]);
+    }
+    sort();
+    findwt();
+    findtat();
+    printGanttChart();
+    findat();
+}
