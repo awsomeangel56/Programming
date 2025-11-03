5f733b11974773ac5f617952b75dd7a7c4a2c260
awsomeangel56 <bilalb162004@gmail.com>
2025-11-03 21:55:55 +0530

    Add files via upload

--- AC LAB/md5.java
+++ AC LAB/md5.java
@@ -1,16 +1,17 @@
 import java.security.*;
 import java.util.*;
 public class md5{
-  public static void main(String[] args) throws Exception{
-    Scanner sc=new Scanner(System.in);
-    System.out.print("Enter message: ");
-    String message=sc.nextLine();
-    MessageDigest md=MessageDigest.getInstance("MD5");
-    byte[] hash=md.digest(message.getBytes("UTF-8"));
-    StringBuilder sb=new StringBuilder();
-    for(byte b:hash){
-      sb.append(String.format("%02x",b));
+    public static void main(String[] args) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter message: ");
+        String message=sc.nextLine();
+        MessageDigest md=MessageDigest.getInstance("MD5");
+        byte[] hash=md.digest(message.getBytes("UTF-8"));
+        System.out.println("MD5 hash: "+Base64.getEncoder().encodeToString(hash));
+        StringBuilder result=new StringBuilder();
+        for(byte b:hash){
+            result.append(String.format("%02x",b));
+        }
+        System.out.print("hex: "+result.toString());
     }
-      System.out.println("MD5 hash: "+sb.toString());
-  }
 }
